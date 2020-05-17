82a7e9550156b8e825c5fef2a823ecf6bb5b8ea1
Bryan English <bryan@bryanenglish.com>
2020-05-17 11:04:42 -0400

    add a test framework

--- test/test.js
+++ test/test.js
@@ -1,47 +1,70 @@
-const assert = require('assert');
-const { getNativeFunction, getBufferPointer } = require('../lib/index');
 const path = require('path');
+const assert = require('assert');
+const pitesti = require('pitesti');
+const { getNativeFunction, getBufferPointer } = require('../lib/index');
 
-const add = getNativeFunction(
-  path.join(__dirname, 'adder', 'libadder.so'),
-  'test_add_uint32_t',
-  'uint32_t',
-  ['uint32_t', 'uint32_t']
-);
+const test = pitesti();
 
-assert.strictEqual(add(0, 2), 2);
-assert.strictEqual(add(4, 5), 9);
-assert.strictEqual(add(11, 22), 33);
-assert.strictEqual(add(Math.pow(2, 32) - 1, 5), 4);
+let add;
+let addPtr;
+let addAsync;
 
-const testBuf = Buffer.alloc(10);
-const testBufPtr = getBufferPointer(testBuf);
-assert.strictEqual(typeof testBufPtr, 'bigint')
-assert(testBufPtr > 0n);
+test`get functions`(() => {
+  add = getNativeFunction(
+    path.join(__dirname, 'adder', 'libadder.so'),
+    'test_add_uint32_t',
+    'uint32_t',
+    ['uint32_t', 'uint32_t']
+  );
 
-const addPtr = getNativeFunction(
-  path.join(__dirname, 'adder', 'libadder.so'),
-  'test_add_ptr_uint32_t',
-  'void',
-  ['uint32_t *', 'uint32_t *', 'uint32_t *']
-);
+  addPtr = getNativeFunction(
+    path.join(__dirname, 'adder', 'libadder.so'),
+    'test_add_ptr_uint32_t',
+    'void',
+    ['uint32_t *', 'uint32_t *', 'uint32_t *']
+  );
 
-const addingBuf = Buffer.alloc(12);
-addingBuf.writeUInt32LE(4);
-addingBuf.writeUInt32LE(3, 4);
-const addingBufPtr = getBufferPointer(addingBuf);
-addPtr(addingBufPtr, addingBufPtr + 4n, addingBufPtr + 8n);
-assert.strictEqual(addingBuf.readUInt32LE(8), 7);
-
-
-const addAsync = getNativeFunction(
-  path.join(__dirname, 'adder', 'libadder.so'),
-  'test_add_async_uint32_t',
-  'void',
-  ['uint32_t', 'uint32_t', ['void', ['uint32_t']]]
-);
-
-addAsync(4, 5, (result) => {
-  assert.strictEqual(result, 9);
-  process.exit(0);
+  addAsync = getNativeFunction(
+    path.join(__dirname, 'adder', 'libadder.so'),
+    'test_add_async_uint32_t',
+    'void',
+    ['uint32_t', 'uint32_t', ['void', ['uint32_t']]]
+  );
 });
+
+test`basic adding`(() => {
+  assert.strictEqual(add(0, 2), 2);
+  assert.strictEqual(add(4, 5), 9);
+  assert.strictEqual(add(11, 22), 33);
+  assert.strictEqual(add(Math.pow(2, 32) - 1, 5), 4);
+});
+
+test`getBufferPointer`(() => {
+  const testBuf = Buffer.alloc(10);
+  const testBufPtr = getBufferPointer(testBuf);
+  assert.strictEqual(typeof testBufPtr, 'bigint')
+  assert(testBufPtr > 0n);
+});
+
+test`adding via pointers`(() => {
+  const addingBuf = Buffer.alloc(12);
+  addingBuf.writeUInt32LE(4);
+  addingBuf.writeUInt32LE(3, 4);
+  const addingBufPtr = getBufferPointer(addingBuf);
+  addPtr(addingBufPtr, addingBufPtr + 4n, addingBufPtr + 8n);
+  assert.strictEqual(addingBuf.readUInt32LE(8), 7);
+})
+
+test`async adding`((done) => {
+  addAsync(4, 5, (result) => {
+    assert.strictEqual(result, 9);
+    done();
+  });
+});
+
+test`promisified adding`(async () => {
+  const addPromise = (a, b) => new Promise(resolve => addAsync(a, b, resolve));
+  assert.strictEqual(await addPromise(5, 3), 8);
+});
+
+test();
