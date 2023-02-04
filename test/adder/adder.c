c96808a92788dce74cb4fafddb8824847f91a2e4
Bryan English <bryan@bryanenglish.com>
2023-02-03 22:15:05 -0500

    test some more types

--- test/adder/adder.c
+++ test/adder/adder.c
@@ -1,20 +1,26 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define mk_adder(t) t test_add_##t(t a, t b) {\
+#define mk_adder(t) mk_adder_type(t, t)
+
+#define mk_adder_type(t, tname) t test_add_##tname(t a, t b) {\
     return a + b;\
   }\
-  void test_add_ptr_##t(t * a, t * b, t * r) {\
+  void test_add_ptr_##tname(t * a, t * b, t * r) {\
     *r = *a + *b;\
   }\
-  void test_add_async_##t(t a, t b, void (*cb)(t)) {\
+  void test_add_async_##tname(t a, t b, void (*cb)(t)) {\
     cb(a + b);\
   }\
-  void test_add_async_twice_##t(t a, t b, void (*cb)(t)) {\
+  void test_add_async_twice_##tname(t a, t b, void (*cb)(t)) {\
     cb(a + b);\
     cb(a + b);\
   }\
 
+mk_adder(int)
+mk_adder(short)
+mk_adder(long)
+mk_adder_type(long long, long_long)
 mk_adder(uint8_t)
 mk_adder(uint16_t)
 mk_adder(uint32_t)
