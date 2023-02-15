0a01b525497b64fdeaa6584aa13397d4aa747abb
Bryan English <bryan@bryanenglish.com>
2023-02-14 22:37:04 -0500

    convert to C++ Node-API
    Also:
    * move test framework to `node:test` polyfill.
    * Workaround for bug where the process stays alive.

--- src/sbffi_dynload.h
+++
@@ -1,6 +0,0 @@
-#include <dynload.h>
-#include "sbffi_common.h"
-
-napi_value js_dlLoadLibrary(napi_env env, napi_callback_info info);
-napi_value js_dlFreeLibrary(napi_env env, napi_callback_info info);
-napi_value js_dlFindSymbol(napi_env env, napi_callback_info info);
--- test/adder/adder.c
+++ test/adder/adder.c
@@ -1,20 +1,40 @@
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 #define mk_adder(t) mk_adder_type(t, t)
 
-#define mk_adder_type(t, tname) t test_add_##tname(t a, t b) {\
+#define mk_adder_type(t, tname) typedef struct {\
+    t a;\
+    t b;\
+    void (*cb)(t);\
+  } thread_data_##tname;\
+  t test_add_##tname(t a, t b) {\
     return a + b;\
   }\
   void test_add_ptr_##tname(t * a, t * b, t * r) {\
     *r = *a + *b;\
   }\
+  void *add_thread_##tname(void * args) {\
+    thread_data_##tname * data = (thread_data_##tname *)args;\
+    t result = data->a + data->b;\
+    void (*cb)(t) = data->cb;\
+    free(data);\
+    cb(result);\
+    return NULL;\
+  }\
   void test_add_async_##tname(t a, t b, void (*cb)(t)) {\
-    cb(a + b);\
+    pthread_t thread;\
+    thread_data_##tname * args = (thread_data_##tname*)malloc(sizeof(thread_data_##tname));\
+    args->a = a;\
+    args->b = b;\
+    args->cb = cb;\
+    pthread_create(&thread, NULL, add_thread_##tname, (void*)args);\
   }\
   void test_add_async_twice_##tname(t a, t b, void (*cb)(t)) {\
-    cb(a + b);\
-    cb(a + b);\
+    test_add_async_##tname(a, b, cb);\
+    test_add_async_##tname(a, b, cb);\
   }\
 
 mk_adder(int)
